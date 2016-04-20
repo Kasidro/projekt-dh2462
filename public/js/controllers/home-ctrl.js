b4b9f17661bd3e876d3069887dc2201ca948a50c
Tobias Johansson <tobias@johansson.xyz>
2016-04-20 23:51:12 +0200

    Add login status feedback

--- public/js/controllers/home-ctrl.js
+++ public/js/controllers/home-ctrl.js
@@ -1,12 +1,16 @@
 magenta.controller('HomeCtrl', function($scope, $window, Planner) {
 
+	$scope.status = 'Welcome, please login';
+
     $scope.login = function() {
+    	$scope.status = 'Loging in...';
     	Planner.login()
     		.then(function(res) {
     			console.log(res);
     			$window.location.href = '/#/start-menu';
     		}).catch(function(err) {
     			console.error(err);
+    			$scope.status = 'Loging failed, please try again';
     		});
     }
 });
