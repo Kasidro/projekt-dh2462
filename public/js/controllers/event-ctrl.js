dea22c9bd97be6e9da8b8df210ac537bda411348
Oscar Eriksson <oerikss@kth.se>
2016-04-21 19:23:37 +0200

    route

--- public/js/controllers/event-ctrl.js
+++ public/js/controllers/event-ctrl.js
@@ -1,19 +1,21 @@
 magenta.controller('EventCtrl', function($scope, $routeParams, Planner) {
-
-
+    
     $scope.id = $routeParams.eventID;
     $scope.mEvent = Planner.getEvent($scope.id);
-    $scope.dayPage = [];
-    $scope.nexpage = function() {};
+    $scope.cPage = [];
+    $scope.page = 1;
+    $scope.nDays;
+    $scope.nexpage = function() {
+        
+    };
     $scope.prevPage = function() {};
 
     (function() {
-        for (var i = 0; i < 3 && i < $scope.mEvent.days.length; i++) {
-            $scope.dayPage.push($scope.mEvent.days[i]);
+        $scope.nDays = $scope.mEvent.days.length;
+        for (var i = 0; i < 3 && i < $scope.nDays; i++) {
+            $scope.cPage.push($scope.mEvent.days[i]);
         }
-        console.log($scope.id);
-        console.log($scope.mEvent);
-        console.log($scope.dayPage);
+        
     })();
 
 });
