13f4675bcf12042bd6868df8fede2cfacaf5b46d
Jakob Fall <Jakob Fall>
2016-04-29 17:47:54 +0200

    changes

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-	url: 'mongodb://mpUserAdmin:trEPE74udAswuhEMu5eyeTrUnu@droplet.johansson.xyz/meeting-planner',
+	url: 'mongodb://mpUserAdmin:BrAfruspavutheFEraM4Kephaf@droplet.johansson.xyz/meeting-planner',
 };
